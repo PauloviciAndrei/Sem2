fdc5e40611fcd623d0bf950578f984e3c58f770b
PauloviciAndrei <itsfazzeliv2@gmail.com>
2025-02-24 23:38:32 +0200

    Delete OOP/a8-9 directory

--- OOP/a8-9/cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_a8_9_FILE D:/Info/Sem2/OOP/a8-9/cmake-build-debug/a8_9.exe)
-set(__QT_DEPLOY_TARGET_a8_9_RUNTIME_DLLS C:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
