9604ad8b2efae2c259350df38924890293ac8175
PauloviciAndrei <itsfazzeliv2@gmail.com>
2025-02-28 22:33:36 +0200

    Delete OOP/Task directory

--- OOP/Task/cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Task_FILE D:/Info/Sem2/OOP/Task/cmake-build-debug/Task.exe)
-set(__QT_DEPLOY_TARGET_Task_RUNTIME_DLLS C:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
